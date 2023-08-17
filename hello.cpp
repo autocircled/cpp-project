7883172de72a1d9816ba06228d4039fb28eb3451
Moktadir Rahman <autocircled@gmail.com>
2023-08-17 21:19:54 +0600

    git init

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    // This is a comment
+    /**
+     * This is a comment
+     * This is a comment
+     * This is a comment
+     */
+    std::cout << "Hello, World!" << '\n';
+    std::cout << "Hello,s Bangladesh!" << '\n';
+    return 0;
+}
